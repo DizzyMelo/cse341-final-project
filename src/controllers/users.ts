99dd1a73731f13587dd1080c2322a39acaf29e0e
eolsenbyui <eugeneolsen@byui.edu>
2022-11-16 07:39:49 -0500

    Change users controller to class-based

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,108 @@
 import express from 'express';
+import { Controller } from './controller'
 
+const implemented: boolean = false;     // Change to true when methods are fully implemented
+
+export class Users implements Controller {
+
+    /////////
+    // POST
+    async post(request: express.Request, response: express.Response): Promise<void> {
+        // #swagger.tagx = ['users']
+        if (implemented) {
+            try {
+                // TODO: Create a new document
+                response.status(201).send();
+            }
+            catch (error: any) {
+                response.status(500).send(error.message);
+            }
+        }
+        else {
+            response.status(501).send('Not yet implemented');
+        }
+    }
+
+    ////////
+    // GET
+    //
+    // getAll returns all records from the collection.
+    async getAll(request: express.Request, response: express.Response): Promise<void> {
+        // #swagger.tagx = ['users']
+        if (implemented) {
+            try {
+                // TODO: Get all documents from this collection
+                response.send();
+            }
+            catch (error: any) {
+                response.status(500).send(error.message);
+            }
+        }
+        else {
+            response.status(501).send('Not yet implemented');
+        }
+    }
+
+    // getOne returns one document specified by the ID parameter
+    async getOne(request: express.Request, response: express.Response): Promise<void> {
+        // #swagger.tagx = ['users']
+        if (implemented) {
+            try {
+                // TODO: Get the document specified by the ID in request.params.id
+                response.send();
+            }
+            catch (error: any) {
+                response.status(500).send(error.message);
+            }
+        }
+        else {
+            response.status(501).send('Not yet implemented');
+        }
+    }
+
+    ////////
+    // PUT
+    async put(request: express.Request, response: express.Response): Promise<void> {
+        // #swagger.tagx = ['users']
+        if (implemented) {
+            try {
+                // TODO: Update the document specified by the ID in request.params.id
+                response.status(204).send();
+            }
+            catch (error: any) {
+                response.status(500).send(error.message);
+            }
+        }
+        else {
+            response.status(501).send('Not yet implemented');
+        }
+    }
+
+    ///////////
+    // DELETE 
+    async delete(request: express.Request, response: express.Response): Promise<void> {
+        // #swagger.tagx = ['users']
+        if (implemented) {
+            try {
+                // TODO: Delete the document specified by the ID in request.params.id
+                response.send();
+            }
+            catch (error: any) {
+                response.status(500).send(error.message);
+            }
+        }
+        else {
+            response.status(501).send('Not yet implemented');
+        }
+    }
+}
+
+/*
 /////////
 // POST
 async function postUser(request: express.Request, response: express.Response): Promise<void> {
     // #swagger.tagx = ['users']
+    response.status(201).send();
     response.status(501).send('Not yet implemented');
 }
 
@@ -14,6 +113,7 @@ async function postUser(request: express.Request, response: express.Response): P
 // getUsers returns all users.
 async function getUsers(request: express.Request, response: express.Response): Promise<void> {
     // #swagger.tagx = ['users']
+    response.send();
     response.status(501).send('Not yet implemented');
 }
 
@@ -21,6 +121,7 @@ async function getUsers(request: express.Request, response: express.Response): P
 // getUser returns one user specified by the ID parameter
 async function getUser(request: express.Request, response: express.Response): Promise<void> {
     // #swagger.tagx = ['users']
+    response.send();
     response.status(501).send('Not yet implemented');
 }
 
@@ -29,6 +130,7 @@ async function getUser(request: express.Request, response: express.Response): Pr
 // PUT
 async function putUser(request: express.Request, response: express.Response): Promise<void> {
     // #swagger.tagx = ['users']
+    response.status(204).send();
     response.status(501).send('Not yet implemented');
 }
 
@@ -37,6 +139,12 @@ async function putUser(request: express.Request, response: express.Response): Pr
 // DELETE 
 async function deleteUser(request: express.Request, response: express.Response): Promise<void> {
     // #swagger.tagx = ['users']
+    try {
+        response.send();
+    }
+    catch (error: any) {
+        response.status(500).send(error.message);
+    }
     response.status(501).send('Not yet implemented');
 }
 
@@ -47,3 +155,4 @@ module.exports = {
     putUser,
     deleteUser
 }
+*/
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 const router = express.Router();
-const controller = require('../controllers/users');
+//const controller = require('../controllers/users');
+import { Users } from '../controllers/users';
 
+const users = new Users();
+
+router.post('/', users.post);
+router.get('/', users.getAll);
+router.get('/:id', users.getOne);
+router.put('/:id', users.put);
+router.delete('/:id', users.delete);
+
+/*
 router.post('/', controller.postUser);
 router.get('/', controller.getUsers);
 router.get('/:id', controller.getUser);
 router.put('/:id', controller.putUser);
 router.delete('/:id', controller.deleteUser);
+*/
 
 module.exports = router;
