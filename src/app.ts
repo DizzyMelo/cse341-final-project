977411ce2e5df2edce26c11f34fd1629d41af63d
eolsenbyui <eugeneolsen@byui.edu>
2022-11-16 18:04:05 -0500

    Add swagger and connect routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import express from 'express';
 const app: express.Application = express();
 
+import * as swaggerUI from 'swagger-ui-express';
+const swaggerDocument = require('../swagger.json')
+
 import mongoose from 'mongoose';
 const port = process.env.PORT || 3000;
 
@@ -10,10 +13,15 @@ app.use(express.json());        // With Express 4.16 and later, we no longer nee
 app.use(express.urlencoded({ extended: true }));  // Must use extended option
 
 app.get('/', (req, res) => {
-  res.send('<h2>The Socratic Method App</h2> <p>See <code>/api-docs</code> endpoint for documentation.</p>');
+  res.send('<h2>The Socratic Method App</h2> <p>See <a href="api-docs">/api-docs</a> endpoint for documentation.</p>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/authorization', require('./routes/auth'));
+app.use('/questions', require('./routes/questions'));
+app.use('/answers', require('./routes/answers'));
+app.use('/comments', require('./routes/comments'));
 app.use('/users', require('./routes/users'));
 
 app.listen(port, () => {
-  return console.log(`Server is running on port ${port}`);
+  return console.log(`The Socratic Method is ready to serve up answers on port ${port}`);
 });
