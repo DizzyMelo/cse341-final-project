d8878b66a3953ea9cef90b3d8c34a1391589a159
eolsenbyui <eugeneolsen@byui.edu>
2022-11-16 07:37:50 -0500

    Add express.json(), urlencoded

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
-const app = express();
+const app: express.Application = express();
 
 import mongoose from 'mongoose';
 const port = process.env.PORT || 3000;
 
+
+app.use(express.json());        // With Express 4.16 and later, we no longer need body-parser.  Express does it.
+app.use(express.urlencoded({ extended: true }));  // Must use extended option
+
 app.get('/', (req, res) => {
-  res.send('Hello, Socratic Method!');
+  res.send('<h2>The Socratic Method App</h2> <p>See <code>/api-docs</code> endpoint for documentation.</p>');
 });
 app.use('/users', require('./routes/users'));
 
