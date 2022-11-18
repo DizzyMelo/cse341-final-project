ba15a35eabcff0b006588164c60c1282ea485d57
eolsenbyui <eugeneolsen@byui.edu>
2022-11-17 20:42:29 -0500

    Add all models to db object

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,18 @@
 import * as mongoose from "mongoose";
-
+// Model definitions:
 import User from './users';
+import Question from './questions';
+import Answer from './answers';
+import Comment from './comments';
+import Post from './posts';
 
 export const db = {
     mongoose: mongoose,
     url: process.env.DB_CONNECT,
-    users: User
-    // TODO: add more models here
+    // Models:
+    users: User,
+    questions: Question,
+    answers: Answer,
+    comments: Comment,
+    posts: Post
 };
