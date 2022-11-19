1b2284a4df6130f318443347c887b07977959ac9
eolsenbyui <eugeneolsen@byui.edu>
2022-11-19 11:02:16 -0500

    Complete Post model

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -2,7 +2,8 @@ import { Schema, Types, model } from 'mongoose';
 
 interface IPost {
     userId: Types.ObjectId;
-    questionId: Types.ObjectId;
+    title: string;
+    question: string;
     answers: Types.ObjectId[];
     timestamp: string;  // Date/Time in ISO 8601 format
     likes: number;
@@ -10,7 +11,8 @@ interface IPost {
 
 const postSchema = new Schema<IPost>({
     userId: { type: Schema.Types.ObjectId, required: true },
-    questionId: { type: Schema.Types.ObjectId, required: true },
+    title: { type: String, required: true },
+    question: { type: String, required: true },
     answers: [{ type: Schema.Types.ObjectId }],
     timestamp: { type: String, required: true },
     likes: { type: Number }
