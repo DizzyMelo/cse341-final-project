a8997972bb1cedba01c87525af11e83a37d89a36
Daniel Melo <daniel.melo42@outlook.com>
2022-11-25 09:01:10 -0700

    auth0 implementation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import { auth } from 'express-openid-connect';
 const app: express.Application = express();
 
 import * as swaggerUI from 'swagger-ui-express';
@@ -10,6 +11,16 @@ import { db } from './models';
 
 const port = process.env.PORT || 3000;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+app.use(auth(config));
 
 app.use(express.json());        // With Express 4.16 and later, we no longer need body-parser.  Express does it.
 app.use(express.urlencoded({ extended: true }));  // Must use extended option
@@ -17,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));  // Must use extended option
 app.get('/', (req, res) => {
   res.send('<h2>The Socratic Method App</h2> <p>See <a href="api-docs">/api-docs</a> endpoint for documentation.</p>');
 });
+
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/authorization', require('./routes/auth'));
 app.use('/posts', require('./routes/posts'));
