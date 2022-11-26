214d4c6a41605536fd9487ce4032f073fd27fb9a
eolsenbyui <eugeneolsen@byui.edu>
2022-11-25 21:37:43 -0500

    Add routes for additional get endpoints

--- src/routes/answers.ts
+++ src/routes/answers.ts
@@ -5,6 +5,7 @@ const controller = require('../controllers/answers');
 router.post('/', controller.post);
 router.get('/', controller.getAll);
 router.get('/:id', controller.getOne);
+router.get('/post/:id', controller.getAnswersForPost);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.deleteOne);
 
