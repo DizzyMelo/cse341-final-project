79a00e1f646d8ad252e81ab39c388edacfec9f2e
eolsenbyui <eugeneolsen@byui.edu>
2022-11-26 22:54:55 -0500

    Point Comment router for answer & post

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -5,8 +5,8 @@ const controller = require('../controllers/comments');
 router.post('/', controller.post);
 router.get('/', controller.getAll);
 router.get('/:id', controller.getOne);
-router.get('/answer/:id', controller.getCommentsForAnswer);
-router.get('/post/:id', controller.getCommentsForPost);
+router.get('/answer/:id', controller.getComments);
+router.get('/post/:id', controller.getComments);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.deleteOne);
 
