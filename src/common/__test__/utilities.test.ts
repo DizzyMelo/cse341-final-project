7f745426607fc5ce308af922f04180be14eecff1
eolsenbyui <eugeneolsen@byui.edu>
2022-11-28 03:19:27 -0500

    Add Jest describe blocks for organization

--- src/common/__test__/utilities.test.ts
+++ src/common/__test__/utilities.test.ts
@@ -8,15 +8,21 @@ let id: ObjectId;
 beforeEach(() => {
     mockClear();
     id = new ObjectId();
-})
-
-test('Valid ID utility - negative', () => {
-    expect(validId('123abc', 'Parent', res)).toBe(false);
-    expect(validId('6381f0f231545a60a133008', 'Post', res)).toBe(false);
-    expect(validId(id.toString() + 'a', 'Parent', res)).toBe(false);
 });
 
-test('Valid ID utility - positive', () => {
-    expect(validId('6381f0f231545a60a133008c', 'Parent', res)).toBe(true);
-    expect(validId(id.toString(), 'User', res)).toBe(true);
+
+describe('Common utility functions', () => {
+    describe('validId() function - check for valid MongoDb ObjectId', () => {
+        test('validId() - negative', () => {
+            expect(validId('123abc', 'Parent', res)).toBe(false);
+            expect(validId('6381f0f231545a60a133008', 'Post', res)).toBe(false);
+            expect(validId(id.toString() + 'a', 'Parent', res)).toBe(false);
+        });
+
+        test('validId() - positive', () => {
+            expect(validId('6381f0f231545a60a133008c', 'Parent', res)).toBe(true);
+            expect(validId(id.toString(), 'User', res)).toBe(true);
+        });
+    });
+    // Add tests for any additional utility functions here.
 });
