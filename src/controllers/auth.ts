641f9304dd1c6fbb6d41b06b1c1971b2cb8f4a15
Daniel Melo <daniel.melo42@outlook.com>
2022-11-30 15:35:24 -0700

    authentication and authorization process implementation

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,19 +1,20 @@
 import express from 'express';
+import appConfig from '../config/app';
 
 const AuthController = {
     login: (request: express.Request, response: express.Response) => {
       // #swagger.tags = ['authorization']
-      /*
+      
       const authorizationURL = `${
-          process.env.AUTHORIZATION_HOST
+        appConfig.authorizationHost
       }/authorize?response_type=code&client_id=${
-          process.env.CLIENT_ID
-      }&redirect_uri=${encodeURIComponent(
-          process.env.REDIRECT_URL
-      )}&scope=openid%20profile%20email`;
-  
+          appConfig.clientId
+        }&redirect_uri=${
+            encodeURIComponent(appConfig.redirectUrl
+              )}&scope=openid%20profile%20email`;
+
       response.redirect(authorizationURL);
-      */
+      
     },
 
     logout: (request: express.Request, response: express.Response) => {
@@ -22,10 +23,24 @@ const AuthController = {
   
     callback: async (request: express.Request, response: express.Response, next: any) => {
       // #swagger.tags = ['authorization']
-      try {
-      } catch (error) {
-        next(error);
-      }
+      
+      const res = await fetch(`${appConfig.authorizationHost}/oauth/token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          grant_type: 'authorization_code',
+          client_id: appConfig.clientId,
+          client_secret: appConfig.clientSecret,
+          redirect_uri: appConfig.redirectUrl,
+          scope: 'openid profile email',
+          code: request.query.code as string,
+        }),
+      });
+    
+      const jsonResponse = await res.json();
+      response.json(jsonResponse);
     },
   };
   
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,6 @@ const controller = require("../controllers/auth");
 
 router.get("/login", controller.login);
 router.get("/logout", controller.logout);
-router.post("/callback", controller.callback);
+router.get("/callback", controller.callback);
 
 module.exports = router;
