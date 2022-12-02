92af2d80756bde3521a8824262ad3666108e0199
eolsenbyui <eugeneolsen@byui.edu>
2022-12-02 04:01:22 -0500

    Eliminate dead ccode.

--- src/controllers/__test__/auth.test.ts
+++ src/controllers/__test__/auth.test.ts
@@ -1,34 +1,11 @@
-import 'dotenv/config';
 import request from "supertest";
 import express from "express";
 import { Server } from "http";
-import mongoose from 'mongoose';
-import { db } from '../../models';
-import { isNullOrUndefined } from 'util';
+import { ObjectId } from 'mongoose';
 
 let server: Server;
-let connection: typeof mongoose;    // Mongoose connection
 
-// Setup and Tear-down
-beforeAll(() => {
-db.mongoose.connect(db.url as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as mongoose.ConnectOptions)
-  .then((value: typeof mongoose) => { 
-    connection = value;
-  })
-  .catch((error: any) => {
-    console.log('Cannot connect to the Socrates database.', error);
-  });
-});
 
-afterAll( async () => {
-    if (connection !== undefined && connection !== null) {
-        await connection.disconnect();
-    }
-});
-  
 beforeEach(() => {
   const app = express();
   app.use('/authorization', require('../../routes/auth'));
@@ -39,7 +16,7 @@ afterEach(() => {
   server.close();
 });
 
-let id: mongoose.ObjectId;
+let id: ObjectId;
 
 
 // Tests
